338c0965435594db7a80aa3f8a0cbf53be6a1e73
RutvikJalodhara <rutvikahir51@gmail.com>
2026-04-11 08:52:20 +0530

    fix: port issue

--- backend/server.js
+++ backend/server.js
@@ -12,4 +12,4 @@ app.use(express.json());
 app.use("/api/shops", require("./routes/shopRoutes"));
 
 const PORT = process.env.PORT ? process.env.PORT : 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
